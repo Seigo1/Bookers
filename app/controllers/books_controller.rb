c8dce97979c9b14e494c3906979ef20e64170751
Seigo1 <champion.1995.08.28@gmal.com>
2021-06-06 18:02:32 +0000

    validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,52 @@
 class BooksController < ApplicationController
+
+  def top
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+    redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+    render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+    redirect_to book_path(book), notice: 'Book was successfully updated.'
+    else
+    render :new
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+    end
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
 end
